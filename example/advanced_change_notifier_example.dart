f6dc597d2b1108853dbe8519d7eb4d0b6d5d92a9
Bilal Demirci <Bilal.demirci001@gmail.com>
2024-06-16 14:14:23 +0100

    Improvement in example

--- example/advanced_change_notifier_example.dart
+++ example/advanced_change_notifier_example.dart
@@ -2,7 +2,7 @@ import 'package:advanced_change_notifier/advanced_change_notifier.dart';
 
 class ChatService extends AdvancedChangeNotifier<String> {
   sendMessage(String text) {
-    print("He said: $text");
+    print("He said: '$text'");
     notifyListeners(value: text);
   }
 }
@@ -11,7 +11,7 @@ void main() {
   var service = ChatService();
   service.addListener(
     (value) async {
-      print("This listener was called and received $value");
+      print("This listener was called and received '$value'");
     },
   );
   service.sendMessage("A test of this package");

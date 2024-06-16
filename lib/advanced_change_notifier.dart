ef6b8c90b6ff4f03b7ccbe23342f95140f51a961
Bilal Demirci <Bilal.demirci001@gmail.com>
2024-06-16 14:13:12 +0100

    Moved classes to /src

--- lib/advanced_change_notifier.dart
+++ lib/advanced_change_notifier.dart
@@ -1,24 +1,3 @@
 library;
 
-typedef AdvancedChangeNotifierCallback<T> = Future<void> Function(T? value);
-
-mixin class AdvancedChangeNotifier<T> {
-  final List<AdvancedChangeNotifierCallback<T>> _listeners = [];
-  notifyListeners({T? value}) {
-    for (var l in _listeners) {
-      l(value);
-    }
-  }
-
-  addListener(AdvancedChangeNotifierCallback<T> callback) {
-    _listeners.add(callback);
-  }
-
-  removeListener(AdvancedChangeNotifierCallback<T> callback) {
-    _listeners.removeWhere(
-      (element) => element == callback,
-    );
-  }
-}
-
-// TODO: Export any libraries intended for clients of this package.
+export 'package:advanced_change_notifier/src/advanced_change_notifier.dart';
